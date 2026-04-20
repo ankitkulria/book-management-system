577bac26262ba06c8fd221e72309b9861829771b
Ankit Suthar <kulriaankit25@gmail.com>
2026-04-20 20:13:02 +0530

    Update AuthorRepo.java

--- src/main/java/com/learning/repository/AuthorRepo.java
+++ src/main/java/com/learning/repository/AuthorRepo.java
@@ -4,7 +4,7 @@ import com.learning.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+@Repository
 public interface AuthorRepo extends JpaRepository<Author,Integer> {
 
     List<Author> findByNameContainingIgnoreCase(String name);
